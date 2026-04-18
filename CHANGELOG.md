d801da2e60d6d3e7fd874009ab963ea8fe65209a
SilentChaos512 <sworkman256@gmail.com>
2026-04-18 11:56:18 -0500

    Change urn color blending to Mixbox algorithm

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.21.1-1.4.8] - 2026-04-18
+### Changed
+- Urn coloring now uses the Mixbox algorithm, requiring Silent Lib 10.6.0+
+
 ## [1.21.1-1.4.7] - 2026-03-17
 ### Changed
 - Increased the size of most urns
--- src/main/java/net/silentchaos512/loginar/crafting/recipe/UrnModificationRecipe.java
+++ src/main/java/net/silentchaos512/loginar/crafting/recipe/UrnModificationRecipe.java
@@ -11,12 +11,16 @@ import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.common.Tags;
 import net.silentchaos512.lib.collection.StackList;
 import net.silentchaos512.lib.util.Color;
+import net.silentchaos512.lib.util.ColorBlendAlgorithm;
+import net.silentchaos512.lib.util.ColorUtils;
 import net.silentchaos512.loginar.block.urn.UrnHelper;
 import net.silentchaos512.loginar.setup.LsDataComponents;
 import net.silentchaos512.loginar.setup.LsRecipeSerializers;
 import net.silentchaos512.loginar.setup.LsTags;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public class UrnModificationRecipe extends CustomRecipe {
@@ -89,52 +93,13 @@ public class UrnModificationRecipe extends CustomRecipe {
         return false;
     }
 
-    // Largely copied from RecipesArmorDyes
     private static void applyDyes(ItemStack urn, Collection<ItemStack> dyes) {
-        int[] componentSums = new int[3];
-        int maxColorSum = 0;
-        int colorCount = 0;
-
-        int clayColor = urn.getOrDefault(LsDataComponents.URN_CLAY_COLOR, UrnHelper.DEFAULT_CLAY_COLOR).getColor();
-        if (clayColor != UrnHelper.DEFAULT_CLAY_COLOR.getColor()) {
-            float r = (float) (clayColor >> 16 & 255) / 255.0F;
-            float g = (float) (clayColor >> 8 & 255) / 255.0F;
-            float b = (float) (clayColor & 255) / 255.0F;
-            maxColorSum = (int) ((float) maxColorSum + Math.max(r, Math.max(g, b)) * 255.0F);
-            componentSums[0] = (int) ((float) componentSums[0] + r * 255.0F);
-            componentSums[1] = (int) ((float) componentSums[1] + g * 255.0F);
-            componentSums[2] = (int) ((float) componentSums[2] + b * 255.0F);
-            ++colorCount;
+        List<Integer> colors = new ArrayList<>();
+        for (var dye : dyes) {
+            getDyeColor(dye).ifPresent(dyeColor -> colors.add(dyeColor.getTextureDiffuseColor()));
         }
-
-        for (ItemStack dye : dyes) {
-            int textureDiffuseColor = getDyeColor(dye)
-                    .orElse(DyeColor.WHITE)
-                    .getTextureDiffuseColor();
-            int r = (textureDiffuseColor >> 16) & 0xFF;
-            int g = (textureDiffuseColor >> 8) & 0xFF;
-            int b = textureDiffuseColor & 0xFF;
-            maxColorSum += Math.max(r, Math.max(g, b));
-            componentSums[0] += r;
-            componentSums[1] += g;
-            componentSums[2] += b;
-            ++colorCount;
-        }
-
-        if (colorCount > 0) {
-            int r = componentSums[0] / colorCount;
-            int g = componentSums[1] / colorCount;
-            int b = componentSums[2] / colorCount;
-            float maxAverage = (float) maxColorSum / (float) colorCount;
-            float max = (float) Math.max(r, Math.max(g, b));
-            r = (int) ((float) r * maxAverage / max);
-            g = (int) ((float) g * maxAverage / max);
-            b = (int) ((float) b * maxAverage / max);
-            int finalColor = (r << 8) + g;
-            finalColor = (finalColor << 8) + b;
-
-            urn.set(LsDataComponents.URN_CLAY_COLOR, new Color(finalColor));
-        }
+        int finalColor = ColorUtils.blend(ColorBlendAlgorithm.MIXBOX, colors);
+        urn.set(LsDataComponents.URN_CLAY_COLOR, new Color(finalColor));
     }
 
     private static Optional<DyeColor> getDyeColor(ItemStack dye) {
